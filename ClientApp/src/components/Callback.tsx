5ebab5f3e4e271fef79ec881d7f1bb9fc6eb670e
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-07-18 23:27:55 +1000

    Auth0 all wired up

---
+++ ClientApp/src/components/Callback.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Callback extends React.Component {
+  public render() {
+
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+}
+
+export default Callback;
